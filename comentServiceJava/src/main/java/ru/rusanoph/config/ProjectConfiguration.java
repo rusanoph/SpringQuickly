79717687362d55109e803d97b9bf6804ce8c1161
Maxim Rusanov <rusanoph@id-r.ru>
2023-12-05 15:36:09 +0300

    Using Spring framework to implement DI. Config with beans.

--- comentServiceJava/src/main/java/ru/rusanoph/config/ProjectConfiguration.java
+++ comentServiceJava/src/main/java/ru/rusanoph/config/ProjectConfiguration.java
@@ -1,10 +1,31 @@
 package ru.rusanoph.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import ru.rusanoph.proxy.CommentNotificatoinProxy;
+import ru.rusanoph.proxy.EmailCommentNotificationProxy;
+import ru.rusanoph.repository.CommentRepository;
+import ru.rusanoph.repository.DBCommentRepository;
+import ru.rusanoph.service.CommentService;
+
 @Configuration
-@ComponentScan(basePackages = {"ru.rusanoph.proxy", "ru.rusanoph.repository", "ru.rusanoph.service"})
 public class ProjectConfiguration {
     
+    @Bean
+    public CommentRepository commentRepository() {
+        return new DBCommentRepository();
+    }
+
+    @Bean
+    public CommentNotificatoinProxy commentNotificatoinProxy() {
+        return new EmailCommentNotificationProxy();
+    }
+
+    @Bean
+    public CommentService commentService(
+        CommentRepository commentRepository,
+        CommentNotificatoinProxy commentNotificatoinProxy) {
+        return new CommentService(commentRepository, commentNotificatoinProxy);
+    }
 }
